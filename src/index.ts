89bd5b9d40564a3c854a6c8b86faa537d3bbd5ba
ivnpotapov <iojikov@mail.ru>
2022-11-09 19:24:24 +0700

    feat: add backend

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import { PORT } from './constants';
+
+import * as serverService from './services/server.service';
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.LOGIN}:${process.env.PASSWORD}@cluster0.k1sfayj.mongodb.net/manager?retryWrites=true&w=majority`,
+    );
+    serverService.server.listen(process.env.PORT || PORT, function () {
+      console.log('Сервер ожидает подключения...');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+process.on('SIGINT', async () => {
+  await mongoose.disconnect();
+  process.exit();
+});
